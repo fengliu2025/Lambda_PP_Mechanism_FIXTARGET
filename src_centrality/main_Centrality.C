79ce860797a0ab910266860d2e632437921a902d
Feng Liu <feng.liu.2@stonybrook.edu>
2025-11-23 15:49:27 -0500

    update makefile

--- src_centrality/main_Centrality.C
+++ src_centrality/main_Centrality.C
@@ -6,7 +6,7 @@ int main(){
 	for (int i =0 ; i < 24;i++){
 		my_cen_analyzer.InputFiles.push_back(Form("/gpfs01/star/scratch/fengliu/JAM2/sqrt_sNN_3p0_root/JAM2_sqrt_3p0_%d.root",i));
 	}
-	my_cen_analyzer.OutputFile = "/Users/feng/Documents/physics_project/Lambda_AuAu/genEvent/JAM1/forFeng_root/charged_multiplicity.root";
+	my_cen_analyzer.OutputFile = "/gpfs01/star/pwg/fliu/Lambda_PP_Mechanism/Lambda_PP_Mechanism_FIXTARGET/scripts/charged_multiplicity.root";
 	my_cen_analyzer.Loop();
 	return 0;
 }
