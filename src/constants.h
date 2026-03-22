cf4a59ec7d05d143296cff796c74144890cb2a74
Feng Liu <fliu5@rcas6008.rcf.bnl.gov>
2026-03-21 22:46:05 -0400

    rapidity

--- src/constants.h
+++ src/constants.h
@@ -37,8 +37,8 @@ const double charged_pion_v1_pt_highCut   = 1.6;
 const double lambda_v1_pt_lowCut        = 0.4;
 const double lambda_v1_pt_highCut       = 2.0;
 
-const double lambda_GlobalPol_y_lowCut      =-0.2 ; 
-const double lambda_GlobalPol_y_highCut     = 1.0 ; 
+const double lambda_GlobalPol_y_lowCut      = -1.0 ; 
+const double lambda_GlobalPol_y_highCut     =  1.0 ; 
 
 const double lambda_GlobalPol_pT_lowCut  = 0.7; 
 //const double lambda_GlobalPol_pT_highCut = 3.0; 
--- src/main_JAM2Analyzer.C
+++ src/main_JAM2Analyzer.C
@@ -10,12 +10,13 @@ int main(){
         //for(int i=0; i<147;i++){
 	//	JAM2_analyzer.InputFiles.push_back(Form("/gpfs01/star/pwg/fliu/temp_root_storage/sqrt_sNN_3p0_root2/JAM2_sqrt_3p0_%d.root",i));
 	//}
-	for (int i = 164; i < 268;i++){
+	for (int i = 0; i < 261;i++){
 		//JAM2_analyzer.InputFiles.push_back(Form("/gpfs01/star/pwg/fliu/temp_root_storage/sqrt_sNN_3p0_root/JAM2_sqrt_3p0_%d.root",i));
-		JAM2_analyzer.InputFiles.push_back(Form("/gpfs01/star/scratch/fengliu/JAM2_ROOT/sqrt3p0/JAM2_sqrt_3p0_%d.root",i));
+		//JAM2_analyzer.InputFiles.push_back(Form("/gpfs01/star/scratch/fengliu/JAM2_ROOT/sqrt3p0/JAM2_sqrt_3p0_%d.root",i));
+		JAM2_analyzer.InputFiles.push_back(Form("/gpfs01/star/pwg/fliu/temp_root_storage/sqrt_sNN_3p0_root/JAM2_sqrt_3p0_%d.root",i));
 	}
 
-	JAM2_analyzer.OutputFile = Form("/gpfs01/star/pwg/fliu/Lambda_PP_Mechanism/Lambda_PP_Mechanism_FIXTARGET/src/JAM2_sqrt_sNN_%.2f_10M_-0.2_1_8.root",JAM2_analyzer.sqrt_sNN);
+	JAM2_analyzer.OutputFile = Form("/gpfs01/star/pwg/fliu/Lambda_PP_Mechanism/Lambda_PP_Mechanism_FIXTARGET/src/JAM2_sqrt_sNN_%.2f_20M_-1.0_1.0_rapidity.root",JAM2_analyzer.sqrt_sNN);
 	JAM2_analyzer.Loop();
 	return 0;
 
