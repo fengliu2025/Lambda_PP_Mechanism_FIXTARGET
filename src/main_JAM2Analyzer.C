f4860b3df09bf0bef223b8d5fc103f935b3a61f3
Feng Liu <fliu5@rcas6008.rcf.bnl.gov>
2026-03-22 12:18:41 -0400

    y 0-1.5

--- src/main_JAM2Analyzer.C
+++ src/main_JAM2Analyzer.C
@@ -16,7 +16,7 @@ int main(){
 		JAM2_analyzer.InputFiles.push_back(Form("/gpfs01/star/pwg/fliu/temp_root_storage/sqrt_sNN_3p0_root/JAM2_sqrt_3p0_%d.root",i));
 	}
 
-	JAM2_analyzer.OutputFile = Form("/gpfs01/star/pwg/fliu/Lambda_PP_Mechanism/Lambda_PP_Mechanism_FIXTARGET/src/JAM2_sqrt_sNN_%.2f_20M_-1.0_1.0_rapidity.root",JAM2_analyzer.sqrt_sNN);
+	JAM2_analyzer.OutputFile = Form("/gpfs01/star/pwg/fliu/Lambda_PP_Mechanism/Lambda_PP_Mechanism_FIXTARGET/src/JAM2_sqrt_sNN_%.2f_20M_-1.5_1.5_rapidity.root",JAM2_analyzer.sqrt_sNN);
 	JAM2_analyzer.Loop();
 	return 0;
 
